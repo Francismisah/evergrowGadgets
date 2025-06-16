e065c3b5035f0ff14a83ba191d1034516c25705c
Francismisah <120067622+Francismisah@users.noreply.github.com>
2025-06-16 10:20:39 -0700

    next

--- frontend/app/(auth)/signUp/login/SignUp.tsx
+++ frontend/app/(auth)/signUp/login/SignUp.tsx
@@ -1,5 +1,4 @@
 "use client";
-import React, { useState } from "react";
 import Button from "./Button";
 import Image from "next/image";
 import Link from "next/link";
--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { NAV_LINKS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
